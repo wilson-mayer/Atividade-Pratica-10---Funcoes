08fc3563e4e1f426081021d063ccc21499e83fd8
Wilson <wr.mayer@live.com>
2024-01-21 16:16:09 -0300

    finished

---
+++ EXERCICIO-06/script.js
@@ -0,0 +1,10 @@
+
+// 6. Crie uma função chamada acessoAoSite() que não tenha
+// parâmetro. Esta função será chamada ao abrir a página e mostrará
+// um alerta informando “Bem vindo ao site”.
+
+function acessoAoSite () {
+    alert("Bem vindo ao site");
+}
+
+acessoAoSite ()
